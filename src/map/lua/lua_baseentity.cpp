14218ebfccbc6b26f714e86f31198638eeff28f7
Davenge <korisd@gmail.com>
2015-03-27 17:34:28 -0400

    started making my own mods

--- src/map/lua/lua_baseentity.cpp
+++ src/map/lua/lua_baseentity.cpp
@@ -144,6 +144,52 @@ inline int32 CLuaBaseEntity::leavegame(lua_State *L)
 }
 
 //==========================================================//
+inline int32 CLuaBaseEntity::AddLinkPearl(lua_State* L)
+{
+    DSP_DEBUG_BREAK_IF(m_PBaseEntity->objtype == TYPE_NPC);
+
+    const int8* linkshellName = lua_tostring(L, 1);
+    const int8* Query = "SELECT name FROM linkshells WHERE name='%s'";
+    int32 ret = Sql_Query(SqlHandle, Query, linkshellName);
+
+    if (ret != SQL_ERROR && Sql_NumRows(SqlHandle) != 0 && Sql_NextRow(SqlHandle) == SQL_SUCCESS)
+    {
+        CCharEntity* PChar = (CCharEntity*)m_PBaseEntity;
+
+        std::string qStr = ("UPDATE char_inventory SET signature='");
+        qStr += linkshellName;
+        qStr += "' WHERE charid = " + std::to_string(PChar->id);
+        qStr += " AND itemId = 515 AND signature = ''";
+        Sql_Query(SqlHandle, qStr.c_str());
+
+        Query = "SELECT linkshellid,color FROM linkshells WHERE name='%s'";
+        ret = Sql_Query(SqlHandle, Query, linkshellName);
+        if (ret != SQL_ERROR && Sql_NumRows(SqlHandle) != 0 && Sql_NextRow(SqlHandle) == SQL_SUCCESS)
+        {
+            CItem* PItem = itemutils::GetItem(515);
+
+            // Update item with name & color //
+            int8 EncodedString[16];
+            EncodeStringLinkshell((int8*)linkshellName, EncodedString);
+            PItem->setSignature(EncodedString);
+            ((CItemLinkshell*)PItem)->SetLSID(Sql_GetUIntData(SqlHandle, 0));
+            ((CItemLinkshell*)PItem)->SetLSColor(Sql_GetIntData(SqlHandle, 1));
+            uint8 invSlotID = charutils::AddItem(PChar, LOC_INVENTORY, PItem);
+
+            // auto-equip it //
+            if (invSlotID != ERROR_SLOTID)
+            {
+                PItem->setSubType(ITEM_LOCKED);
+                PChar->equip[SLOT_LINK1] = invSlotID;
+                PChar->equipLoc[SLOT_LINK1] = LOC_INVENTORY;
+                linkshell::AddOnlineMember(PChar, (CItemLinkshell*)PItem, 1);
+            }
+        }
+    }
+
+    return 1;
+}
+//==========================================================//
 
 inline int32 CLuaBaseEntity::ChangeMusic(lua_State *L)
 {
@@ -9572,6 +9618,7 @@ const int8 CLuaBaseEntity::className[] = "CBaseEntity";
 
 Lunar<CLuaBaseEntity>::Register_t CLuaBaseEntity::methods[] =
 {
+    LUNAR_DECLARE_METHOD(CLuaBaseEntity, AddLinkPearl),
     LUNAR_DECLARE_METHOD(CLuaBaseEntity,ChangeMusic),
     LUNAR_DECLARE_METHOD(CLuaBaseEntity,warp),
     LUNAR_DECLARE_METHOD(CLuaBaseEntity,leavegame),
--- src/map/lua/lua_baseentity.h
+++ src/map/lua/lua_baseentity.h
@@ -77,6 +77,7 @@ public:
         return m_PBaseEntity;
     }
 
+    int32 AddLinkPearl(lua_State *L);
     int32 ChangeMusic(lua_State* L);        // Sets the specified music Track for specified music block.
 
     int32 warp(lua_State*);                 // Returns Character to home point
