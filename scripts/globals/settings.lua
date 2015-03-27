73ab6b57a4326746b36a6771452ef1dc4f315e06
Davenge <korisd@gmail.com>
2015-03-27 11:52:31 -0400

    quetz rebirth changes

--- scripts/globals/settings.lua
+++ scripts/globals/settings.lua
@@ -16,8 +16,8 @@
 require("scripts/globals/common");
 
 -- Enable Extension (1= yes 0= no)
-ENABLE_COP     = 0;
-ENABLE_TOAU    = 0;
+ENABLE_COP     = 1;
+ENABLE_TOAU    = 1;
 ENABLE_WOTG    = 0;
 ENABLE_ACP     = 0;
 ENABLE_AMK     = 0;
@@ -32,37 +32,37 @@ ENABLE_SOA     = 0;
 RESTRICT_BY_EXPANSION = 0;
 
 -- CHARACTER CONFIG
-INITIAL_LEVEL_CAP = 50; -- The initial level cap for new players.  There seems to be a hardcap of 255.
+INITIAL_LEVEL_CAP = 75; -- The initial level cap for new players.  There seems to be a hardcap of 255.
 MAX_LEVEL = 75; -- Level max of the server, lowers the attainable cap by disabling Limit Break quests.
 NORMAL_MOB_MAX_LEVEL_RANGE_MIN = 81; -- Lower Bound of Max Level Range for Normal Mobs (0 = Uncapped)
 NORMAL_MOB_MAX_LEVEL_RANGE_MAX = 84; -- Upper Bound of Max Level Range for Normal Mobs (0 = Uncapped)
-START_GIL = 10; --Amount of gil given to newly created characters.
-START_INVENTORY = 30; -- Starting inventory and satchel size.  Ignores values < 30.  Do not set above 80!
+START_GIL = 10000; --Amount of gil given to newly created characters.
+START_INVENTORY = 80; -- Starting inventory and satchel size.  Ignores values < 30.  Do not set above 80!
 OPENING_CUTSCENE_ENABLE = 0; --Set to 1 to enable opening cutscenes, 0 to disable.
 SUBJOB_QUEST_LEVEL = 18; -- Minimum level to accept either subjob quest.  Set to 0 to start the game with subjobs unlocked.
 ADVANCED_JOB_LEVEL = 30; -- Minimum level to accept advanced job quests.  Set to 0 to start the game with advanced jobs.
-ALL_MAPS = 0; -- Set to 1 to give starting characters all the maps.
-UNLOCK_OUTPOST_WARPS = 0; -- Set to 1 to give starting characters all outpost warps.  2 to add Tu'Lia and Tavnazia.
+ALL_MAPS = 1; -- Set to 1 to give starting characters all the maps.
+UNLOCK_OUTPOST_WARPS = 1; -- Set to 1 to give starting characters all outpost warps.  2 to add Tu'Lia and Tavnazia.
 
-SHOP_PRICE = 1.000; -- Multiplies prices in NPC shops.
+SHOP_PRICE = 0.750; -- Multiplies prices in NPC shops.
 GIL_RATE   = 1.000; -- Multiplies gil earned from quests.  Won't always display in game.
-EXP_RATE   = 1.000; -- Multiplies exp earned from fov.
-TABS_RATE  = 1.000; -- Multiplies tabs earned from fov.
-SAN_FAME   = 1.000; -- Multiplies fame earned from San d'Oria quests.
-BAS_FAME   = 1.000; -- Multiplies fame earned from Bastok quests.
-WIN_FAME   = 1.000; -- Multiplies fame earned from Windurst quests.
-NORG_FAME  = 1.000; -- Multiplies fame earned from Norg and Tenshodo quests.
-JEUNO_FAME = 1.000; -- Multiplies fame earned from Jeuno quests.
+EXP_RATE   = 2.000; -- Multiplies exp earned from fov.
+TABS_RATE  = 1.500; -- Multiplies tabs earned from fov.
+SAN_FAME   = 4.000; -- Multiplies fame earned from San d'Oria quests.
+BAS_FAME   = 4.000; -- Multiplies fame earned from Bastok quests.
+WIN_FAME   = 4.000; -- Multiplies fame earned from Windurst quests.
+NORG_FAME  = 4.000; -- Multiplies fame earned from Norg and Tenshodo quests.
+JEUNO_FAME = 4.000; -- Multiplies fame earned from Jeuno quests.
 
-FISHING_GUILD_POINTS      = 1.000; -- Multiplies guild points earned from fishermans' guild trades.
-WOODWORKING_GUILD_POINTS  = 1.000; -- Multiplies guild points earned from carpenters' guild trades.
-SMITHING_GUILD_POINTS     = 1.000; -- Multiplies guild points earned from blacksmiths' guild trades.
-GOLDSMITHING_GUILD_POINTS = 1.000; -- Multiplies guild points earned from goldsmiths' guild trades.
-CLOTHCRAFT_GUILD_POINTS   = 1.000; -- Multiplies guild points earned from weavers' guild trades.
-LEATHERCRAFT_GUILD_POINTS = 1.000; -- Multiplies guild points earned from tanners' guild trades.
-BONECRAFT_GUILD_POINTS    = 1.000; -- Multiplies guild points earned from boneworkers' guild trades.
-ALCHEMY_GUILD_POINTS      = 1.000; -- Multiplies guild points earned from alchemists' guild trades.
-COOKING_GUILD_POINTS      = 1.000; -- Multiplies guild points earned from culinarians' guild trades.
+FISHING_GUILD_POINTS      = 2.000; -- Multiplies guild points earned from fishermans' guild trades.
+WOODWORKING_GUILD_POINTS  = 2.000; -- Multiplies guild points earned from carpenters' guild trades.
+SMITHING_GUILD_POINTS     = 2.000; -- Multiplies guild points earned from blacksmiths' guild trades.
+GOLDSMITHING_GUILD_POINTS = 2.000; -- Multiplies guild points earned from goldsmiths' guild trades.
+CLOTHCRAFT_GUILD_POINTS   = 2.000; -- Multiplies guild points earned from weavers' guild trades.
+LEATHERCRAFT_GUILD_POINTS = 2.000; -- Multiplies guild points earned from tanners' guild trades.
+BONECRAFT_GUILD_POINTS    = 2.000; -- Multiplies guild points earned from boneworkers' guild trades.
+ALCHEMY_GUILD_POINTS      = 2.000; -- Multiplies guild points earned from alchemists' guild trades.
+COOKING_GUILD_POINTS      = 2.000; -- Multiplies guild points earned from culinarians' guild trades.
 DISABLE_GUILD_CONTRACTS   = 0; -- Set to 1 to disable guild contracts, allowing players to accumulate guild points from all guilds at once.
 
 CURE_POWER  = 1.000; -- Multiplies amount healed from Healing Magic, including the relevant Blue Magic.
@@ -74,14 +74,14 @@ WEAPON_SKILL_POWER  = 1.000; -- Multiplies damage dealt by Weapon Skills.
 WEAPON_SKILL_POINTS = 1.000; -- Multiplies points earned during weapon unlocking.
 USE_ADOULIN_WEAPON_SKILL_CHANGES = false; -- true/false. Change to toggle new Adoulin weapon skill damage calculations
 
-HARVESTING_BREAK_CHANCE = 0.33; -- % chance for the sickle to break during harvesting.  Set between 0 and 1.
-EXCAVATION_BREAK_CHANCE = 0.33; -- % chance for the pickaxe to break during excavation.  Set between 0 and 1.
-LOGGING_BREAK_CHANCE    = 0.33; -- % chance for the hatchet to break during logging.  Set between 0 and 1.
-MINING_BREAK_CHANCE     = 0.33; -- % chance for the pickaxe to break during mining.  Set between 0 and 1.
-HARVESTING_RATE         = 0.50; -- % chance to recieve an item from haresting.  Set between 0 and 1.
-EXCAVATION_RATE         = 0.50; -- % chance to recieve an item from excavation.  Set between 0 and 1.
-LOGGING_RATE            = 0.50; -- % chance to recieve an item from logging.  Set between 0 and 1.
-MINING_RATE             = 0.50; -- % chance to recieve an item from mining.  Set between 0 and 1.
+HARVESTING_BREAK_CHANCE = 0.13; -- % chance for the sickle to break during harvesting.  Set between 0 and 1.
+EXCAVATION_BREAK_CHANCE = 0.13; -- % chance for the pickaxe to break during excavation.  Set between 0 and 1.
+LOGGING_BREAK_CHANCE    = 0.13; -- % chance for the hatchet to break during logging.  Set between 0 and 1.
+MINING_BREAK_CHANCE     = 0.13; -- % chance for the pickaxe to break during mining.  Set between 0 and 1.
+HARVESTING_RATE         = 0.70; -- % chance to recieve an item from haresting.  Set between 0 and 1.
+EXCAVATION_RATE         = 0.70; -- % chance to recieve an item from excavation.  Set between 0 and 1.
+LOGGING_RATE            = 0.70; -- % chance to recieve an item from logging.  Set between 0 and 1.
+MINING_RATE             = 0.70; -- % chance to recieve an item from mining.  Set between 0 and 1.
 
 -- SE implemented coffer/chest illusion time in order to prevent coffer farming. No-one in the same area can open a chest or coffer for loot (gil, gems & items)
 -- till a random time between MIN_ILLSION_TIME and MAX_ILLUSION_TIME. During this time players can loot keyitem and item related to quests (AF, maps... etc.)
@@ -98,8 +98,8 @@ LandKingSystem_HQ = 2;
 -- DYNAMIS SETTINGS
     BETWEEN_2DYNA_WAIT_TIME = 1;        -- wait time between 2 Dynamis (in real day) min: 1 day
              DYNA_LEVEL_MIN = 65;       -- level min for entering in Dynamis
-    TIMELESS_HOURGLASS_COST = 500000;   -- cost of the timeless hourglass for Dynamis.
-     CURRENCY_EXCHANGE_RATE = 100;      -- X Tier 1 ancient currency -> 1 Tier 2, and so on.  Certain values may conflict with shop items.  Not designed to exceed 198.
+    TIMELESS_HOURGLASS_COST = 100000;   -- cost of the timeless hourglass for Dynamis.
+     CURRENCY_EXCHANGE_RATE = 90;       -- X Tier 1 ancient currency -> 1 Tier 2, and so on.  Certain values may conflict with shop items.  Not designed to exceed 198.
 RELIC_2ND_UPGRADE_WAIT_TIME = 604800;      -- wait time for 2nd relic upgrade (stage 2 -> stage 3) in seconds. 604800s = 1 RL week.
 RELIC_3RD_UPGRADE_WAIT_TIME = 295200;      -- wait time for 3rd relic upgrade (stage 3 -> stage 4) in seconds. 295200s = 82 hours.
 FREE_COP_DYNAMIS = 1 ; -- Authorize player to entering inside COP Dynamis without completing COP mission ( 1 = enable 0= disable)
@@ -139,7 +139,7 @@ DIA_OVERWRITE = 1; --Set to 1 to allow Bio to overwrite same tier Dia.  Default 
 BIO_OVERWRITE = 0; --Set to 1 to allow Dia to overwrite same tier Bio.  Default is 0.
 BARELEMENT_OVERWRITE = 1; --Set to 1 to allow Barelement spells to overwrite each other (prevent stacking).  Default is 1.
 BARSTATUS_OVERWRITE = 1; --Set to 1 to allow Barstatus spells to overwrite each other (prevent stacking).  Default is 1.
-BARD_SONG_LIMIT = 1; --Maximum amount of songs from a single Bard that can be granted to a single target at once.  Set between 1 and 31.
+BARD_SONG_LIMIT = 2; --Maximum amount of songs from a single Bard that can be granted to a single target at once.  Set between 1 and 31.
 BARD_INSTRUMENT_LIMIT = 2; --Maximum amount of songs from a single Bard with an instrument that can be granted to a single target at once.  Set between 2 and 32.
 ENHANCING_SONG_DURATION = 120; -- duration of enhancing bard songs such as Minuets, Ballads, etc.
 STONESKIN_CAP = 350; -- soft cap for hp absorbed by stoneskin
@@ -157,17 +157,17 @@ USE_OLD_CURE_FORMULA = false; -- true/false. if true, uses older cure formula (3
 
 -- CELEBRATIONS
 EXPLORER_MOOGLE = 1; -- Enables Explorer Moogle teleports
-EXPLORER_MOOGLE_LEVELCAP = 10;
-JINX_MODE_2005 = 0; -- Set to 1 to give starting characters swimsuits from 2005.  Ex: Hume Top
-JINX_MODE_2008 = 0; -- Set to 1 to give starting characters swimsuits from 2008.  Ex: Custom Top
-JINX_MODE_2012 = 0; -- Set to 1 to give starting characters swimsuits from 2012.  Ex: Marine Top
-SUMMERFEST_2004 = 0; -- Set to 1 to give starting characters Far East dress from 2004.  Ex: Onoko Yukata
-SUNBREEZE_2009 = 0; -- Set to 1 to give starting characters Far East dress from 2009.  Ex: Otokogusa Yukata
-SUNBREEZE_2011 = 0; -- Set to 1 to give starting characters Far East dress from 2011.  Ex: Hikogami Yukata
-CHRISTMAS = 0; -- Set to 1 to give starting characters Christmas dress.
-HALLOWEEN = 0; -- Set to 1 to give starting characters Halloween items (Does not start event).
-HALLOWEEN_2005 = 0; -- Set to 1 to Enable the 2005 version of Harvest Festival, will start on Oct. 20 and end Nov. 1.
-HALLOWEEN_YEAR_ROUND = 0; -- Set to 1 to have Harvest Festival initialize outside of normal times.
+EXPLORER_MOOGLE_LEVELCAP = 1;
+JINX_MODE_2005 = 1; -- Set to 1 to give starting characters swimsuits from 2005.  Ex: Hume Top
+JINX_MODE_2008 = 1; -- Set to 1 to give starting characters swimsuits from 2008.  Ex: Custom Top
+JINX_MODE_2012 = 1; -- Set to 1 to give starting characters swimsuits from 2012.  Ex: Marine Top
+SUMMERFEST_2004 = 1 -- Set to 1 to give starting characters Far East dress from 2004.  Ex: Onoko Yukata
+SUNBREEZE_2009 = 1; -- Set to 1 to give starting characters Far East dress from 2009.  Ex: Otokogusa Yukata
+SUNBREEZE_2011 = 1; -- Set to 1 to give starting characters Far East dress from 2011.  Ex: Hikogami Yukata
+CHRISTMAS = 1; -- Set to 1 to give starting characters Christmas dress.
+HALLOWEEN = 1; -- Set to 1 to give starting characters Halloween items (Does not start event).
+HALLOWEEN_2005 = 1; -- Set to 1 to Enable the 2005 version of Harvest Festival, will start on Oct. 20 and end Nov. 1.
+HALLOWEEN_YEAR_ROUND = 1; -- Set to 1 to have Harvest Festival initialize outside of normal times.
 
 -- MISC
 HOMEPOINT_HEAL = 0; --Set to 1 if you want Home Points to heal you like in single-player Final Fantasy games.
