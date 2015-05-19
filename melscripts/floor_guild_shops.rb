7eed32736f7ed723af85fc2770b19db548e9aa07
Davenge <korisd@gmail.com>
2015-05-19 11:37:44 -0400

    added guild shop changes rb and sql script

---
+++ melscripts/floor_guild_shops.rb
@@ -0,0 +1,24 @@
+#!/usr/bin/ruby
+
+require 'mysql'
+
+begin
+
+  con = Mysql.new 'localhost', 'darkstar', 'Grc937!', 'dspdb'
+  rs = con.query "SELECT guildid, itemid, min_price, max_price FROM guild_shops;"
+
+  f = File.open( '../sql/guild_shop_change.sql', 'w' )
+
+  rs.each_hash() do |row|  
+     if( ( row['max_price'] = row['max_price'].to_i() * 0.33 ) < row['min_price'].to_i() ) 
+        row['max_price'] = row['min_price'].to_i()
+     end
+     f.write "UPDATE guild_shops SET max_price=#{row['max_price'].to_i()} WHERE guildid=#{row['guildid'].to_i()} and itemid=#{row['itemid'].to_i()};\n"
+  end
+  f.close
+rescue Mysql::Error => e
+   puts e.errno
+   puts e.error
+ensure
+   con.close if con
+end
