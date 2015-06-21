bd3fb14df66b74d0bd4cfa0ec2ddcd4d84ad683b
dphillips2014 <desilyn90@gmail.com>
2015-06-20 22:18:36 -0500

    Added Divine Veil AOE functionality.

--- sql/melfsql/rhos_changes.sql
+++ sql/melfsql/rhos_changes.sql
@@ -1,8 +1,9 @@
 -- Tarasque's Spawn Point
 UPDATE mob_spawn_points SET pos_x=124, pos_y=19, pos_z=163, pos_rot=100 WHERE mobname="Tarasque";
 
--- Habetrot's Spawn Point
-UPDATE mob_spawn_points SET pos_x=-60, pos_y=-8, pos_z=56, pos_rot=220 WHERE mobname="Habetrot";
+-- Updates spell_list.sql so that "Divine Veil"  works (added AOE = 7 for Divine Veil)
+-- (These spells also work with SCH's Accession, so SCH needs to check for both AOE=4 and AOE=7)
+UPDATE spell_list SET AOE=7 WHERE name="poisona" OR name="paralyna" OR name="silena" OR name="blindna" OR name="cursna" OR name="stona" OR name="erase";
 
 -- Correct droplist for Thread Leeches (in Pashhow Marshlands, zoneID = 109)
 -- (poolid = 5133; vial of beastman's blood = 930; dropid = 3472; vial of fiend blood = 924)
@@ -13,3 +14,10 @@ UPDATE mob_spawn_points SET pos_x=-137, pos_y=7.57, pos_z=146 WHERE mobId=168224
 
 -- Buff the Dragon Family (+STR, +Elemental Resistance)
 UPDATE mob_family_system SET STR=3, Light=.9, Dark=.75, Fire=.9, Ice=.9, Water=.9, Lightning=.9, Earth=.9, Wind=.9 WHERE family="Dragon";
+
+-- This Rumble Crawler should only spawn in place of Habetrot
+-- Gave it a new group in mob_groups and changed it to forced spawn
+-- INSERT INTO mob_spawn_mob VALUES (17428812,0,17,1);
+
+-- Habetrot's Spawn Point
+UPDATE mob_spawn_points SET pos_x=-60, pos_y=-8, pos_z=58, pos_rot=220 WHERE mobname="Habetrot";
--- src/map/utils/battleutils.cpp
+++ src/map/utils/battleutils.cpp
@@ -4474,6 +4474,16 @@ void assistTarget(CCharEntity* PChar, uint16 TargID)
 
 uint8 GetSpellAoEType(CBattleEntity* PCaster, CSpell* PSpell)
 {
+	CCharEntity* PChar = (CCharEntity*)PCaster;
+
+	if (PSpell->getAOE() == SPELLAOE_DIVINE_VEIL)	
+		// Divine Veil spells are also Accession spells
+        if (PCaster->StatusEffectContainer->HasStatusEffect(EFFECT_ACCESSION)
+			|| (PCaster->StatusEffectContainer->HasStatusEffect(EFFECT_DIVINE_SEAL) 
+			&& charutils::hasTrait(PChar, TRAIT_DIVINE_VEIL)))
+            return SPELLAOE_RADIAL;
+        else
+            return SPELLAOE_NONE;
     if (PSpell->getAOE() == SPELLAOE_RADIAL_ACCE)
         if (PCaster->StatusEffectContainer->HasStatusEffect(EFFECT_ACCESSION))
             return SPELLAOE_RADIAL;
