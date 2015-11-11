3a9939d237e6914af12824e63304c4cdd0aab345
Davenge <korisd@gmail.com>
2015-11-11 00:53:25 -0500

    made it non-specific nation vendor

--- scripts/zones/Port_Windurst/npcs/Taniko-Maniko.lua
+++ scripts/zones/Port_Windurst/npcs/Taniko-Maniko.lua
@@ -24,26 +24,26 @@ function onTrigger(player,npc)
     player:showText(npc,TANIKOMANIKO_SHOP_DIALOG);
 
     stock = {
-        0x4181,  2542,1,     --Brass Zaghnal
-        0x4302,  7128,1,     --Wrapped Bow
-        0x43AB,   162,1,     --Ice Arrow
-        0x43AC,   162,1,     --Lightning Arrow
+        0x4181,  2542,     --Brass Zaghnal
+        0x4302,  7128,     --Wrapped Bow
+        0x43AB,   162,     --Ice Arrow
+        0x43AC,   162,     --Lightning Arrow
 
-        0x4015,   104,2,     --Cat Baghnakhs
-        0x4001,   129,2,     --Cesti
-        0x4109,  5864,2,     --Bone Pick
-        0x4301,   482,2,     --Self Bow
-        0x43A6,     3,2,     --Wooden Arrow
-        0x439C,    54,2,     --Hawkeye
-        0x4380,  1575,2,     --Boomerang
+        0x4015,   104,     --Cat Baghnakhs
+        0x4001,   129,     --Cesti
+        0x4109,  5864,     --Bone Pick
+        0x4301,   482,     --Self Bow
+        0x43A6,     3,     --Wooden Arrow
+        0x439C,    54,     --Hawkeye
+        0x4380,  1575,     --Boomerang
 
-        0x4102,  4198,3,     --Bone Axe
-        0x4180,   309,3,     --Bronze Zaghnal
-        0x41C0,    97,3,     --Harpoon
-        0x4300,    39,3,     --Shortbow
-        0x43A7,     4,3      --Bone Arrow
+        0x4102,  4198,     --Bone Axe
+        0x4180,   309,     --Bronze Zaghnal
+        0x41C0,    97,     --Harpoon
+        0x4300,    39,     --Shortbow
+        0x43A7,     4,     --Bone Arrow
     }
-    showNationShop(player, WINDURST, stock);
+    showShop(player, STATIC, stock);
 
 end; 
 
