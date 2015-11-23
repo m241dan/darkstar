c4aac049225735f6787d97c2ca6558e662ea19f5
Davenge <korisd@gmail.com>
2015-11-23 16:49:48 -0500

    upgraded the swirling_vortex in zalkurm dunes to do a CoP skip

--- scripts/zones/Valkurm_Dunes/npcs/Swirling_Vortex.lua
+++ scripts/zones/Valkurm_Dunes/npcs/Swirling_Vortex.lua
@@ -23,6 +23,50 @@ end;
 -----------------------------------
 
 function onTrigger(player,npc)
+
+   if( ZilartProgress >= THE_GATE_OF_THE_GODS and ZilartProgress ~= 255 and player:getVar( "CoPSkip" ) == 0 ) then
+      player:completeMission( COP, ANCIENT_FLAMES_BECKON );
+      player:completeMission( COP, THE_RITES_OF_LIFE );
+      player:completeMission( COP, BELOW_THE_ARKS );
+      player:completeMission( COP, THE_MOTHERCRYSTALS );
+      player:completeMission( COP, AN_INVITATION_WEST );
+      player:completeMission( COP, THE_LOST_CITY );
+      player:completeMission( COP, DISTANT_BELIEFS );
+      player:completeMission( COP, AN_ETERNAL_MELODY );
+      player:completeMission( COP, ANCIENT_VOWS );
+      player:completeMission( COP, A_TRANSIENT_DREAM );
+      player:completeMission( COP, THE_CALL_OF_THE_WYRMKING );
+      player:completeMission( COP, A_VESSEL_WITHOUT_A_CAPTAIN );
+      player:completeMission( COP, THE_ROAD_FORKS );
+      player:completeMission( COP, DESCENDANTS_OF_A_LINE_LOST );
+      player:completeMission( COP, COMEDY_OF_ERRORS_ACT_I );
+      player:completeMission( COP, TENDING_AGED_WOUNDS );
+      player:completeMission( COP, DARKNESS_NAMED );
+      player:completeMission( COP, SHELTERING_DOUBT );
+      player:completeMission( COP, THE_SAVAGE );
+      player:completeMission( COP, THE_SECRETS_OF_WORSHIP );
+      player:completeMission( COP, SLANDEROUS_UTTERINGS );
+      player:completeMission( COP, THE_ENDURING_TUMULT_OF_WAR );
+      player:completeMission( COP, DESIRES_OF_EMPTINESS );
+      player:completeMission( COP, THREE_PATHS );
+      player:completeMission( COP, PARTNERS_WITHOUT_FAME );
+      player:completeMission( COP, SPIRAL );
+      player:completeMission( COP, WHERE_MESSENGERS_GATHER );
+      player:completeMission( COP, FLAMES_FOR_THE_DEAD );
+      player:completeMission( COP, FOR_WHOM_THE_VERSE_IS_SUNG );
+      player:completeMission( COP, A_PLACE_TO_RETURN );
+      player:completeMission( COP, MORE_QUESTIONS_THAN_ANSWERS );
+      player:completeMission( COP, ONE_TO_BE_FEARED );
+      player:completeMission( COP, CHAINS_AND_BONDS );
+      player:completeMission( COP, FLAMES_IN_THE_DARKNESS );
+      player:completeMission( COP, FIRE_IN_THE_EYES_OF_MEN );
+      player:completeMission( COP, CALM_BEFORE_THE_STORM );
+      player:completeMission(COP,THE_WARRIOR_S_PATH);
+      player:addMission(COP,GARDEN_OF_ANTIQUITY);
+      player:setVar("PromathiaStatus",0);
+      player:setVar( "CoPSkip", 1 );
+   end
+
 	
 	if (player:hasCompletedMission(COP,THE_MOTHERCRYSTALS)) then
 		player:startEvent(0x000c);
