4f60e6f9b659d7564b171e984fc214051f699201
Davenge <korisd@gmail.com>
2016-02-19 11:15:43 -0500

    revamp of augment system complete

--- scripts/globals/melfaugments.lua
+++ scripts/globals/melfaugments.lua
@@ -1,8 +1,8 @@
 -- Abyssea Augs --
+local ITEM_DAGGER = 16450
+local augmentable_items = {}
 
-augmentable_items = {}
-
-augment_strings = { 
+local augment_strings = { 
                     [1] = "HP", [9] = "MP", [31] = "Evasion", [33] = "Defense", [39] = "Enmity", [41] = "Crit Hit Rate", [42] = "Enemy Crit Hit Rate -", [51] = "HP recovered while resting",
                     [52] = "MP recovered while resting", [53] = "Spell Interruption Down", [54] = "Phyiscal Dmg. Taken -", [55] = "Magic Dmg. Taken -", [56] = "Breath Dmg. Taken -", [57] = "Magic Crit Rate",
                     [133] = "Magic Attack Bonus", [137] = "Regen", [138] = "Refresh", [140] = "Fast Cast", [141] = "Conserve MP", [147] = "Treasure Hunter", [148] = "Gilfinder", [153] = "Shield Mastery",
@@ -84,11 +84,11 @@ augmentable_items[16265] = { { 9, 1, 4   }, -- wivre gorget, wiki order
                              { 41, 0, 1  },
                              { 772, 2, 3 } };
 
-aug_chances = { 100, 70, 50, 20 };
+local aug_chances = { 100, 70, 50, 20 };
 
 -- Skahnowa || Eraser --
 
-general_pool = {
+local general_pool = {
                  { 768, 0, 4 }, -- resistances
                  { 769, 0, 4 },
                  { 770, 0, 4 },
@@ -97,36 +97,20 @@ general_pool = {
                  { 773, 0, 4 },
                  { 774, 0, 4 },
                  { 775, 0, 4 },
-                 { 512, 0, 0 }, -- +2 stats
-                 { 513, 0, 0 },
-                 { 514, 0, 0 },
-                 { 515, 0, 0 },
-                 { 516, 0, 0 },
-                 { 517, 0, 0 },
-                 { 518, 0, 0 },
                  { 1, 0, 14  }, -- HP 1-15
                  { 9, 0, 14  }, -- MP 1-15
+                 { 18,  0, 0 }, -- hp mp +33
 }
-
-one_time_pool = {
-                  { 512, 0, 1 }, -- stat +2
-                  { 513, 0, 1 },
-                  { 514, 0, 1 },
-                  { 515, 0, 1 },
-                  { 516, 0, 1 },
-                  { 517, 0, 1 },
-                  { 518, 0, 1 },
-                  { 18,  0, 0 }, -- hp mp +33
-                  { 134, 0, 2 }, -- magic defense
-                  { 133, 0, 1 }, -- magic atk bonus
-                  { 23,  0, 4 }, -- acc
-                  { 25,  0, 4 }, -- attack
-                  { 31,  0, 9 }, -- evasion
-}
+local gen_size = table.getn( general_pool );
  
-mob_specific_pool = {};
+local mob_specific_pool = {};
 -- skahnowa
-mob_specific_pool[17301593] = { { 133, 0, 1 }, -- magic atk bonus
+mob_specific_pool[17301593] = {
+                                { 516, 0, 1 }, -- INT + 1-2
+                                { 517, 0, 1 }, -- WIS + 1-2
+                                { 518, 0, 1 }, -- CHR +1-2
+                                { 134, 0, 2 }, -- magic defense
+                                { 133, 0, 1 }, -- magic atk bonus
                                 { 35, 0, 2  }, -- magic acc
                                 { 323, 0, 2 }, -- Cure Casting Time
                                 { 329, 0, 2 }, -- cure potency
@@ -140,8 +124,12 @@ mob_specific_pool[17301593] = { { 133, 0, 1 }, -- magic atk bonus
 }
 
 -- eraser
-mob_specific_pool[17310106] = { { 25, 0, 4  }, -- atk
-                                { 23, 0, 4  }, -- acc
+mob_specific_pool[17310106] = {
+                                { 512, 0, 1 }, -- STR +2
+                                { 513, 0, 1 }, -- DEX +2
+                                { 514, 0, 1 }, -- AGI +2
+                                { 25, 0, 3  }, -- atk
+                                { 23, 0, 3  }, -- acc
                                 { 41, 0, 0  }, -- crit chance
                                 { 328, 0, 2 }, -- crit hit dmg
                                 { 143, 0, 0 }, -- double atk
@@ -152,8 +140,11 @@ mob_specific_pool[17310106] = { { 25, 0, 4  }, -- atk
                                 { 211, 0, 0 }, -- snapshot
                                 { 39, 0, 2  }, -- +enmity
                                 { 330, 0, 2 }, -- waltz potency
+                                { 31,  0, 9 }, -- evasion
                                 { 109, 0, 1 }, -- pet: dbl atk +1 crit +1
 }
+
+local mob_size = { [17301593] = table.getn( mob_specific_pool[17301593] ), [17310106] = table.getn( mob_specific_pool[17310106] ) }
 
 function onAugmentID(player, trade)
    local item = trade:getItemObj();
@@ -176,16 +167,62 @@ end;
 function onAugmentTrade(player, trade)
    -- sanity checks
    if( trade:getItemCount() ~= 2 ) then
-      player:PrintToPlayer( "You need to trade an item and a gil amount.", 0xE );
+      player:PrintToPlayer( "You need to trade an item and a gil amount; or an augmented dagger and an augmentable item", 0xE );
       return;
    end
+
+   local gil = trade:getGil();
+   if( gil == 0 ) then
+      local item_one = trade:getItem(0);
+      local item_two = trade:getItem(1);
+      local dagger, item_to_aug
+      if( item_one == ITEM_DAGGER ) then
+         dagger = trade:getItemObj(0);
+         item_to_aug = trade:getItemObj(1);
+      elseif( item_two == ITEM_DGGER ) then
+         dagger = trade:getItemObj(1);
+         item_to_aug = trade:getItemObj(0);
+      else
+         printf( "This one..." );
+         player:PrintToPlayer( "You need to trade an item and a gil amount; or an augmented dagger and an augmentable item", 0xE );
+         return;
+      end
+
+      if( item_to_aug:isSubType(4) == true ) then
+         player:PrintToPlayer( "That item is already augmented, go cleanse it first!", 0xE );
+         return;
+      end
+
+      if( item_to_aug:isType(0x08) == false and item_to_aug:isType(0x10) == false ) then
+         player:PrintToPlayer( "You may only augment weapons or armor.", 0xE );
+         return;
+      end
+      if( bit.band(item_to_aug:getFlag(), 16384 ) == 16384 ) then
+         player:PrintToPlayer( "You cannot augment an EX item.", 0xE );
+         return;      
+      end
+
+      if( bit.band(item_to_aug:getFlag(), 32768 ) == 32768 ) then
+         player:PrintToPlayer( "You cannot augment a RARE item.", 0xE );
+         return;
+      end 
+      local aug1, aug1val = dagger:getAugment(0);
+      local aug2, aug2val = dagger:getAugment(1);
+      local aug3, aug3val = dagger:getAugment(2);
+      local aug4, aug4val = dagger:getAugment(3);
+
+      player:addItem( item_to_aug:getID(), 1, aug1, aug1val, aug2, aug2val, aug3, aug3val, aug4, aug4val );
+      player:tradeComplete()
+      player:PrintToPlayer( "You plunge the dagger into the item and it melts on contact, enchanting its augments unto the item.", 0xE );
+      return;
+   end 
 
    local itemID = trade:getItem(1);
    if( augmentable_items[itemID] == nil ) then
       local amount = trade:getGil();
       local itemObj = trade:getItemObj(1);
       local itemID = itemObj:getID()
-      if( itemID ~= 16450 ) then
+      if( itemID ~= ITEM_DAGGER ) then
          player:PrintToPlayer( "This item is not Augmentable", 0xE );
       else
          player:query( string.format( "UPDATE char_inventory SET bazaar=%d WHERE charid=%d and location=%d and slot=%d and itemId=%d", amount, player:getID(), itemObj:getLocationID(), itemObj:getSlotID(), itemObj:getID() ) );
@@ -200,7 +237,6 @@ function onAugmentTrade(player, trade)
       return;
    end
 
-   local gil = trade:getGil();
    if( gil ~= 1 and gil ~= 2 ) then
       player:PrintToPlayer( "You traded an improper gil amount.", 0xE );
       return;
@@ -261,10 +297,6 @@ end;
 
 
 function onFieldParchmentTrade(npc, player, trade)
-   if( player:getVar( string.format("%dItemTraded", npc:getID() ) ) ~= 0 ) then
-      player:PrintToPlayer( "You've already traded an item.", 0xE );
-      return;
-   end
    local lasttrade = player:getVar( string.format( "%dlasttrade", npc:getID() ) );
 
    if( lasttrade > os.time(t) ) then
@@ -272,29 +304,8 @@ function onFieldParchmentTrade(npc, player, trade)
       return;
    end 
 
-   if( trade:getItemCount() ~= 2 ) then
-      player:PrintToPlayer( "You need to trade a gil amount and an item", 0xE );
-      return;
-   end
-
-
-   local itemObj = trade:getItemObj(1);
-   if( itemObj:isSubType(4) == true ) then
-      player:PrintToPlayer( "That item is already augmented.", 0xE );
-      return;
-   end
-
-   if( itemObj:isType(0x08) == false and itemObj:isType(0x10) == false ) then
-      player:PrintToPlayer( "You may only augment weapons or armor.", 0xE );
-      return;
-   end
-   if( bit.band(itemObj:getFlag(), 16384 ) == 16384 ) then
-      player:PrintToPlayer( "You cannot augment an EX item.", 0xE );
-      return;      
-   end
-
-   if( bit.band(itemObj:getFlag(), 32768 ) == 32768 ) then
-      player:PrintToPlayer( "You cannot augment a RARE item.", 0xE );
+   if( trade:getItemCount() ~= 1 ) then
+      player:PrintToPlayer( "You need to trade a gil amount in increments of 5k to the max of 20k.", 0xE );
       return;
    end
 
@@ -303,6 +314,7 @@ function onFieldParchmentTrade(npc, player, trade)
       player:PrintToPlayer( "Improper gil value traded.", 0xE );
       return;
    end
+
    gil = gil / 5000;
    local availableaugs = player:getVar( "AugBuys" );
    if( availableaugs < gil ) then
@@ -310,16 +322,7 @@ function onFieldParchmentTrade(npc, player, trade)
       return;
    end
 
-   player:setVar( string.format( "%dItemTraded", npc:getID() ), itemObj:getID() );
    player:setVar( string.format( "%dNumAugs", npc:getID() ), gil );
-
-   if( itemObj:getAHCat() == 13 ) then
-      player:setVar( string.format( "%dItemType", npc:getID() ), 3 );
-   elseif( itemObj:isType(8) == true ) then
-      player:setVar( string.format( "%dItemType", npc:getID() ), 1 );
-   else
-      player:setVar( string.format( "%dItemType", npc:getID() ), 2 );
-   end
 
    player:setVar( "AugBuys", availableaugs - gil );
    player:tradeComplete();
@@ -334,69 +337,65 @@ function onFieldParchmentTrade(npc, player, trade)
    npc:setStatus(2);
 end;
 
+function getAugFromPool( pool )
+   return pool[1], math.random( pool[2], pool[3] );
+end;
+
+function addAugToPool( pool, aug, val )
+   if( pool[aug] ) then
+      pool[aug] = pool[aug] + val;
+   else
+      pool[aug] = val;
+   end
+end;
+
 function onFieldParchmentTrigger(npc, player)
-   if( player:getVar( string.format( "%dItemTraded", npc:getID() ) ) == 0 ) then
-      player:PrintToPlayer( "You need to trade an augmentable item, have AugBuys and gil first!", 0xE );
+   local NPCID = npc:getID()
+   local num_augs = player:getVar( NPCID .. "NumAugs" );
+   local lasttrade = player:getVar( string.format( "%dlasttrade", npc:getID() ) );
+
+   if( lasttrade > os.time(t) and num_augs == 0 ) then
+      player:PrintToPlayer( string.format( "You need to wait %d minutes before you can trade again.", (lasttrade - os.time(t))/60), 0xE );
+      return;
+   end 
+
+   if( num_augs == 0 ) then
+      player:PrintToPlayer( "You need to trade an gil first(5k,10k,15k,20k) and have the AugBuys!", 0xE );
       return;
    end
 
-   local generalPool = {};
-   local itemPool = item_pool[player:getVar( string.format( "%dItemType", npc:getID() ) )];
-   local mobSpecificPool = mob_specific_pool[npc:getID()];
-   local selectedAugs = {};
-   local augPool = { [1] = {}, [2] = {}, [3] = {}, [4] = {} };
-   local itemObj = GetItemPtrByID( player:getVar( string.format( "%dItemTraded", npc:getID() ) ) );
-   -- setup the general pool --
-
-   for i,v in ipairs( general_pool ) do
-      table.insert( generalPool, 1, v );
-   end
-
-   for i,v in ipairs( itemPool ) do
-      table.insert( generalPool, 1, v );
-   end
-
-   -- build the aug pool
-   table.insert( augPool[1], 1, generalPool );
-   table.insert( augPool[1], 1, one_time_pool );
-   table.insert( augPool[1], 1, mobSpecificPool );
-   table.insert( augPool[2], 1, generalPool );
-   table.insert( augPool[2], 1, mobSpecificPool );
-   table.insert( augPool[3], 1, mobSpecificPool );
-   table.insert( augPool[4], 1, mobSpecificPool );
-
-   -- loop "NumAugs" times for the augs
-   for i=1, player:getVar( string.format( "%dNumAugs", npc:getID() ) ), 1 do
-      local augGroup;
+   local mob_pool = mob_specific_pool[NPCID];
+   local augments = {}
+   local pick, aug, val
+   -- grab out augs and put them into the "augments" table
+   for i=1, num_augs, 1 do
       if( math.random(1,100) <= aug_chances[i] ) then
-         augGroup = augPool[i][math.random(1,table.getn( augPool[i]))];
-         aug = augGroup[math.random(1,table.getn(augGroup))]
-         local augval;
-         if( aug[2] == aug[3] ) then
-            augval = aug[2];
-         else
-            augval = math.random(aug[2], aug[3]);
+         if( i < 3 ) then -- dilute the first two pools with general
+            pick = math.random( 1, gen_size + mob_size[NPCID] );
+            if( pick < gen_size ) then
+               aug, val = getAugFromPool( general_pool[pick] );
+            else
+               aug, val = getAugFromPool( mob_pool[pick - gen_size] )
+            end
+         else -- last two rolls, if successful are undiluted
+            pick = math.random( 1, mob_size[NPCID] );
+            aug, val = getAugFromPool( mob_pool[pick] )
          end
-         if( itemObj:getAHCat() == 24 or itemObj:getAHCat() == 25 or itemObj:getAHCat() == 15 ) then
-            augval = augval / 2
-         end
-         if( selectedAugs[aug[1]] ~= nil ) then
-            selectedAugs[aug[1]] = selectedAugs[aug[1]] + augval; 
-         else
-            selectedAugs[aug[1]] = augval;
-         end
+         addAugToPool( augments, aug, val );
       end
    end
-   local augID = {};
-   local augVal = {};
-   for i,v in pairs( selectedAugs ) do
-      local position = table.getn(augID);
-      augID[position+1] = i;
-      augVal[position+1] = v;
+
+   -- distribute our "augments" into friendly passing manner
+   local augVal = {}
+   local augID = {}
+   local count = 1
+   for k,v in pairs( augments ) do
+      augID[count] = k;
+      augVal[count] = v;
+      count = count + 1;
    end
+
    player:PrintToPlayer( "Good luck!", 0xE );
-   player:addItem( player:getVar( string.format( "%dItemTraded", npc:getID() ) ), 1, augID[1], augVal[1], augID[2], augVal[2], augID[3], augVal[3], augID[4], augVal[4] );
-   player:setVar( string.format( "%dItemTraded", npc:getID() ), 0 );
-   player:setVar( string.format( "%dNumAugs", npc:getID() ), 0 );
-   player:setVar( string.format( "%dItemType", npc:getID() ), 0 );
+   player:addItem( ITEM_DAGGER, 1, augID[1], augVal[1], augID[2], augVal[2], augID[3], augVal[3], augID[4], augVal[4] );
+   player:setVar( string.format( "%dNumAugs", NPCID ), 0 );
 end;
