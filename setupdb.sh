bd82dbf3829db2e16afc44746d4e13a9dda20a9b
Davenge <korisd@gmail.com>
2016-08-13 17:18:09 -0400

    added char_points

--- setupdb.sh
+++ setupdb.sh
@@ -10,7 +10,7 @@ SQL="SET group_concat_max_len = 10240;"
 SQL="${SQL} SELECT GROUP_CONCAT(table_name separator ' ')"
 SQL="${SQL} FROM information_schema.tables WHERE table_schema='${DBTODUMP}'"
 SQL="${SQL} AND table_name IN ('account_ip_record','accounts','accounts_banned','auction_house','audit_chat','char_blacklist','char_effects','char_equip','char_exp',"
-SQL="${SQL}'char_inventory','char_jobs','char_look','char_merit','char_pet','char_pet_name','char_profile','char_recast','char_skills','char_spells','char_stats',"
+SQL="${SQL}'char_inventory','char_jobs','char_points','char_look','char_merit','char_pet','char_pet_name','char_profile','char_recast','char_skills','char_spells','char_stats',"
 SQL="${SQL}'char_storage','char_style','char_vars','char_weapon_skill_points','chars','delivery_box','conquest_system','linkshells','server_variables');"
 
 TBLIST=`mysql -u $DBADMIN -p$DBPASS -AN -e"${SQL}"`
