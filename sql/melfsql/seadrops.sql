f5c8b54c7c8081b7c308969469fb86d108bf8cad
Davenge <korisd@gmail.com>
2016-08-13 15:57:43 -0400

    sea drops added

---
+++ sql/melfsql/seadrops.sql
@@ -0,0 +1,9 @@
+UPDATE `mob_droplist` SET rate=0 WHERE itemID=15508; -- justice torque
+UPDATE `mob_droplist` SET rate=0 WHERE itemID=15509;
+UPDATE `mob_droplist` SET rate=0 WHERE itemID=15510;
+UPDATE `mob_droplist` SET rate=0 WHERE itemID=15511;
+UPDATE `mob_droplist` SET rate=0 WHERE itemID=15512;
+UPDATE `mob_droplist` SET rate=0 WHERE itemID=15513;
+UPDATE `mob_droplist` SET rate=0 WHERE itemID=15514; -- love torque
+
+
